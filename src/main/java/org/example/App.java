0cce27de99dfb3cbe0aaa236f40ed6594b364372
Pavel Doldin <doldin86@icloud.com>
2024-12-19 19:03:16 +0300

    Add_ez1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,34 @@
 package org.example;
 
 
-public class App 
-{
+import java.util.Scanner;
+
+public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        ez1();
+    }
+
+    public static void ez1(){
+        /**
+         *  Напишите программу, которая складывает, вычитает, умножает, делит два целых числа.
+         *  И выводит результат.
+         */
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ведите первое число: ");
+        int firstNumber = scanner.nextInt();
+        System.out.print("Введите второе число: ");
+        int secondNumber = scanner.nextInt();
+        int res1 = firstNumber + secondNumber;
+        int res2 = firstNumber - secondNumber;
+        int res3 = firstNumber * secondNumber;
+        double res4 = firstNumber / secondNumber;
+        System.out.println(firstNumber + " + " + secondNumber + " = " + res1);
+        System.out.println(firstNumber + " - " + secondNumber + " = " + res2);
+        System.out.println(firstNumber + " * " + secondNumber + " = " + res3);
+        System.out.println(firstNumber + " / " + secondNumber + " = " + res4);
+
+
     }
 }
+
