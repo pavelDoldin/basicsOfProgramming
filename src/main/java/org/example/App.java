3a88c3fefd9ba89383140a7efb373f9f8b151035
Pavel Doldin <doldin86@icloud.com>
2024-12-22 08:41:56 +0300

    Add_ex3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,10 +5,34 @@ import java.util.Scanner;
 
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-        ex2();
+        //System.out.println( "Hello World!" );
+        ex3();
     }
+    public static void ex3(){
+        /**
+         *  Создайте 5 переменных типа String: dumbledore, mcgonagall, snape, lupin, hugrid.
+         *  Присвойте каждой переменной значение, соответствующее ее названию. Должны получиться поля,
+         *  содержащие имена знаменитых волшебников.
+         * Затем создайте еще одну строку с названием teachers, содержащую текст: “School teachers:“.
+         * Обратите внимание, в тексте содержится двоеточие.
+         * Выведите на экран сначала строку teachers, затем все 5 строк с именами преподавателей.
+         * Должен получиться список учителей.
+         */
 
+        String dumbledore = "dumbledore";
+        String mcgonagall = "mcgonagall";
+        String snape = "snape";
+        String lupin = "lupin";
+        String hugrid = "hugrid";
+        String teachers = "School teachers:";
+        System.out.println(teachers);
+        System.out.println(dumbledore);
+        System.out.println(mcgonagall);
+        System.out.println(snape);
+        System.out.println(lupin);
+        System.out.println(hugrid);
+
+    }
     public static void ex2(){
         /*
          * Напишите программу, которая проверяет.
